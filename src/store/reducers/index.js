fffcf7d43eaac4ff0025dc86d1132411c48a83e1
rsfdev <roger_faustino@hotmail.com>
2020-06-10 18:00:52 -0300

    Conflict resolution

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,15 +1,8 @@
 import { combineReducers } from 'redux';
-<<<<<<< HEAD
 import reducerTeste from './reducerTeste';
 
 const rootReducer = () => combineReducers({
   reducerTeste,
-=======
-import ReducerLogin from './ReducerLogin';
-
-const rootReducer = () => combineReducers({
-  ReducerLogin,
->>>>>>> f67f7a1e0c188b217666dd591baf475a827d1d12
 });
 
 export default rootReducer;
