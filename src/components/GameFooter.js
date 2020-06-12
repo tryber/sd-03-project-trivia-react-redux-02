162b3459649d389c1b30b7f874c519cfd602a87e
Lucas Werner <lucas.1werner@gmail.com>
2020-06-12 15:58:16 -0300

    cc

--- src/components/GameFooter.js
+++ src/components/GameFooter.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import propTypes from 'prop-types';
 
 class GameFooter extends Component {
   constructor(props) {
