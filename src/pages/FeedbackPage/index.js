0d44d24b700d1cf6dce1793305a1b73bd8ca16e6
Gabryel <gabryel.ryba@gmail.com>
2020-06-16 17:40:12 -0300

    Refactored Feedback page render method

--- src/pages/FeedbackPage/index.js
+++ src/pages/FeedbackPage/index.js
@@ -5,17 +5,47 @@ import { bindActionCreators } from 'redux';
 import Header from '../../components/GameHeader';
 
 class Feedback extends Component {
-  static renderFeedbackMessage(assertions) {
+  static renderFeedbackMessage() {
+    const stringfiedState = localStorage.getItem('state');
+    const state = JSON.parse(stringfiedState);
+    const { player: { assertions } } = state;
     if (assertions < 3) {
       return 'Podia ser melhor...';
     }
     return 'Mandou bem!';
   }
 
-  render() {
+  static renderFeedbackInfo() {
     const stringfiedState = localStorage.getItem('state');
     const state = JSON.parse(stringfiedState);
     const { player: { assertions, score } } = state;
+    return (
+      <div>
+        <p data-testid="feedback-text">{Feedback.renderFeedbackMessage(assertions)}</p>
+        <p data-testid="feedback-total-score">{`Pontuação: ${score}`}</p>
+        <p data-testid="feedback-total-question">{`Número de acertos: ${assertions}`}</p>
+      </div>
+    );
+  }
+
+  static renderButtons() {
+    return (
+      <div>
+        <Link to="/">
+          <button data-testid="btn-play-again" type="button">
+            Jogar novamente
+          </button>
+        </Link>
+        <Link to="/Ranking">
+          <button data-testid="btn-ranking" type="button">
+            Ver Ranking
+          </button>
+        </Link>
+      </div>
+    );
+  }
+
+  render() {
     return (
       <div
         style={{
@@ -27,23 +57,8 @@ class Feedback extends Component {
         }}
       >
         <Header />
-        <div>
-          <p data-testid="feedback-text">{Feedback.renderFeedbackMessage(assertions)}</p>
-          <p data-testid="feedback-total-score">{`Pontuação: ${score}`}</p>
-          <p data-testid="feedback-total-question">{`Número de acertos: ${assertions}`}</p>
-        </div>
-        <div>
-          <Link to="/">
-            <button data-testid="btn-play-again" type="button">
-              Jogar novamente
-            </button>
-          </Link>
-          <Link to="/Ranking">
-            <button data-testid="btn-ranking" type="button">
-              Ver Ranking
-            </button>
-          </Link>
-        </div>
+        {Feedback.renderFeedbackInfo()}
+        {Feedback.renderButtons()}
       </div>
     );
   }
