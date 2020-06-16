97c00fafa83a9231030f3b9cb8128a875665d06d
Gabryel <gabryel.ryba@gmail.com>
2020-06-16 17:21:41 -0300

    estrutura inicial feedback page

--- src/pages/FeedbackPage/index.js
+++ src/pages/FeedbackPage/index.js
@@ -1,20 +1,49 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Header from '../../components/GameHeader';
 
 class Feedback extends Component {
+  static renderFeedbackMessage(assertions) {
+    if (assertions < 3) {
+      return 'Podia ser melhor...';
+    }
+    return 'Mandou bem!';
+  }
+
   render() {
+    const stringfiedState = localStorage.getItem('state');
+    const state = JSON.parse(stringfiedState);
+    const { player: { assertions, score } } = state;
     return (
       <div
         style={{
           display: 'flex',
           justifyContent: 'center',
+          flexDirection: 'column',
+          textAlign: 'center',
           width: '600px',
         }}
       >
-        INICIANDO FEEDBACK PAGE
         <Header />
+        <div>
+          <p data-testid="feedback-text">{Feedback.renderFeedbackMessage(assertions)}</p>
+          <p data-testid="feedback-total-score">{`Pontuação: ${score}`}</p>
+          <p data-testid="feedback-total-question">{`Número de acertos: ${assertions}`}</p>
+        </div>
+        <div>
+          <Link to="/">
+            <button data-testid="btn-play-again" type="button">
+              Jogar novamente
+            </button>
+          </Link>
+          <Link to="/Ranking">
+            <button data-testid="btn-ranking" type="button">
+              Ver Ranking
+            </button>
+          </Link>
+        </div>
       </div>
     );
   }
