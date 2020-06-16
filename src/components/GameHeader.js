7761fb25ababd0707e291e8027b0c45e52ab3c5b
Lucas Werner <lucas.1werner@gmail.com>
2020-06-16 19:17:05 -0300

    falta css do ranking

--- src/components/GameHeader.js
+++ src/components/GameHeader.js
@@ -9,20 +9,21 @@ class GameHeader extends Component {
   }
 
   render() {
-    const { name, gravatarEmail, score } = this.props;
+    const { player } = JSON.parse(localStorage.getItem('state'));
+    const { score } = this.props;
     return (
       <header className="card-header">
         <div className="card-header-title">
           <div className="media">
             <div className="media-left">
               <img
-                src={gravatarEmail}
+                src={player.gravatarEmail}
                 alt="profile-pic"
                 className="image is-48x48"
                 data-testid="header-profile-picture"
               />
             </div>
-            <p className="title is-6" data-testid="header-player-name">{`Jogador: ${name}`}</p>
+            <p className="title is-6" data-testid="header-player-name">{`Jogador: ${player.name}`}</p>
             <div className="media-right">
               <p className="subtitle is-6" data-testid="header-score">
                 {`Pontos: ${score}`}
--- src/pages/RankingPage/index.js
+++ src/pages/RankingPage/index.js
@@ -1,11 +1,107 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
+import 'bulma/css/bulma.css';
 
-export default class Ranking extends Component {
+class Ranking extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  restartGame() {
+    console.log(this.props);
+    localStorage.removeItem('state');
+  }
+
+  renderRankingList() {
+    console.log(this.props);
+    const ranking = JSON.parse(localStorage.getItem('ranking'));
+    return (
+      <ol>
+        {ranking.map(({ name, gravatarEmail, score }, index) => (
+          <li>
+            <div>
+              <img className="image is-25x25" src={gravatarEmail} alt="profile-pic" />
+              <p data-testid={`player-name-${index}`}>{`${name} - `}</p>
+              <p data-testid={`player-score-${index}`}>{`${score} pontos`}</p>
+            </div>
+          </li>
+        ))}
+      </ol>
+    );
+  }
+
+  renderGoHomeButton() {
+    console.log(this.props);
+    return (
+      <Link to="/">
+        <button
+          type="button"
+          className="button is-info card-footer-item"
+          data-testid="btn-go-home"
+          onClick={() => this.restartGame()}
+        >
+          JOGAR NOVAMENTE
+        </button>
+      </Link>
+    );
+  }
+
   render() {
     return (
-      <div>
-        Teste
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <div
+          className="card"
+          style={{
+            width: '1',
+          }}
+        >
+          <div className="card-header-title is-centered" data-testid="ranking-title">
+            Ranking
+          </div>
+          <div className="card-content">
+            {this.renderRankingList()}
+          </div>
+          <div className="card-content">
+            {this.renderGoHomeButton()}
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+
+const mapStateToProps = ({
+  ReducerPlayer: {
+    name,
+    gravatarEmail,
+    score,
+  },
+}) => ({
+  name,
+  gravatarEmail,
+  score,
+});
+
+const mapDispatchToProps = (dispatch) => bindActionCreators(
+  {
+  }, dispatch,
+);
+
+Ranking.propTypes = {
+  name: PropTypes.string.isRequired,
+  gravatarEmail: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Ranking);
